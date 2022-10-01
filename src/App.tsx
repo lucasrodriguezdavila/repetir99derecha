20ee7f4d42f9f0549c587174b24334ded66f5967
Julian Feregotti <julian.feregotti@snappler.com>
2022-10-01 19:44:53 -0300

    refactored code and added path track

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import Globo from "./components/Globo/Globo";
+import Globo from "./components/RealTimeTrack/RealTimeTrack";
+import { IssPathGlobe } from "./components/IssPathGlobe/IssPathGlobe";
 import { MainNavbar } from "./components/MainNavbar";
+import { useEffect, useState } from "react";
+import { SatRec, twoline2satrec } from "satellite.js";
+import { SatelliteTLEResponse } from "./services/types/satelliteTleResponse";
+import { getSatelliteByID } from "./services/SatelliteService";
 
 function App() {
   const satelliteId = 25544;
 
+  const [satRec, setSatRec] = useState<SatRec>();
+  useEffect(() => {
+    getSatelliteByID(satelliteId).then((sat) => {
+      const data = sat as unknown as SatelliteTLEResponse;
+      if (data.line1) {
+        const satRec = twoline2satrec(data.line1, data.line2);
+        setSatRec(satRec);
+      }
+    });
+  }, [satelliteId]);
+
   return (
     <div className="App">
       <MainNavbar />
-      <Globo satelliteId={satelliteId} />
+      {satRec && <IssPathGlobe satelliteId={satelliteId} satRec={satRec} />}
     </div>
   );
 }
--- src/helpers/SatellitePositionHelper.ts
+++ src/helpers/SatellitePositionHelper.ts
@@ -6,8 +6,9 @@ import {
   SatRec,
 } from "satellite.js";
 import { EARTH_RADIUS_KM } from "../constants";
-import { radiansToDegrees } from "../helpers/radiansToDegrees";
+import { radiansToDegrees } from "./RadiansToDegrees";
 import { SatelliteCoordinates } from "../interfaces/Satellite";
+import moment from "moment";
 
 const buildSatellitePostion = (
   satrec: SatRec,
@@ -24,4 +25,20 @@ const buildSatellitePostion = (
   };
 };
 
-export { buildSatellitePostion };
+const buildPathsBetweenDates = (
+  satrec: SatRec,
+  startDate: Date,
+  endDate: Date
+) => {
+  if (!satrec) return [];
+  const paths = [];
+  let currentDate = startDate;
+  while (currentDate < endDate) {
+    const path = buildSatellitePostion(satrec, currentDate);
+    paths.push([path.latitude, path.longitude, path.altitude]);
+    currentDate = moment(currentDate).add(1, "minutes").toDate();
+  }
+  return [paths];
+};
+
+export { buildSatellitePostion, buildPathsBetweenDates };
