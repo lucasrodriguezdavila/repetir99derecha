4ac8fdf6300b5759ec5b35a2ca3363e29f922c04
Lucas <lucasrodriguezdavila@gmail.com>
2022-10-01 15:59:20 -0300

    added main navigator

--- src/components/MainNavbar/index.tsx
+++ src/components/MainNavbar/index.tsx
@@ -5,8 +5,16 @@ export const MainNavbar: React.FC<Props> = () => {
   return (
     <div className="main-navbar">
       <ul className="main-navbar-ul">
-        <li>
+        <li className="main-navbar-menu-item">
           <button className="main-navbar-button">ISS</button>
+          <ul className="main-navbar-dropdown">
+            <li>
+              <a href="/">Risk</a>
+              <a href="/">Risk</a>
+              <a href="/">Risk</a>
+              <a href="/">Risk</a>
+            </li>
+          </ul>
         </li>
         <li>
           <button className="main-navbar-button">ISS</button>
