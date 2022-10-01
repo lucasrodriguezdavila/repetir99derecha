a5b8719d6e1abf0867bfb8b4c76607c1b2f0c34d
Lucas <lucasrodriguezdavila@gmail.com>
2022-10-01 15:59:20 -0300

    added main navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Globo from "./components/Globo/Globo";
+import { MainNavbar } from "./components/MainNavbar";
 
 function App() {
   const satelliteId = 25544;
   return (
     <div className="App">
+      <MainNavbar />
       <Globo satelliteId={satelliteId} />
     </div>
   );
