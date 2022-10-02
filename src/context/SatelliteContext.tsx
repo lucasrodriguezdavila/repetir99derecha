2717fb8b37a07a404daef7c4c5dfc2ae8f179e05
Lucas <lucasrodriguezdavila@gmail.com>
2022-10-01 21:35:46 -0300

    added satellite tracking context

--- src/context/SatelliteContext.tsx
+++ src/context/SatelliteContext.tsx
@@ -4,6 +4,8 @@ import { Satellite } from "../interfaces/Satellite";
 interface ISatelliteContext {
   satellites: Satellite[];
   setSatellites: React.Dispatch<React.SetStateAction<Satellite[]>>;
+  isISSTracked: boolean;
+  setIsISSTracked: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const SatelliteContext = createContext<ISatelliteContext>(null!);
@@ -17,12 +19,15 @@ export const SatelliteProvider = ({
   children: React.ReactNode;
 }) => {
   const [satellites, setSatellites] = React.useState<Satellite[]>([]);
+  const [isISSTracked, setIsISSTracked] = React.useState<boolean>(false);
 
   return (
     <SatelliteContext.Provider
       value={{
         satellites,
         setSatellites,
+        isISSTracked,
+        setIsISSTracked,
       }}
     >
       {children}
